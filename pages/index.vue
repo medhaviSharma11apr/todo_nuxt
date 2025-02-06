50f49331d710147d0f212b1d14f5af7aab2b06dd
medhaviSharma11apr <medhaviSharma11apr@gmail.com>
2025-02-06 16:25:43 +0530

    new commit

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,22 @@
         </div>
         <div class="container mx-auto flex gap-2 ">
 
-            <input type="text" v-model="newTask"  @keyup.enter="addTask()" class="border rounded p-2 w-full">
+            <input type="text" v-model="newTask" @keyup.enter="addTask()" class="border rounded p-2 w-full">
             <button @click="addTask()" class="bg-blue-500 text-white px-4 py-2 rounded">Add</button>
 
+        </div>
+
+        <div class="container   mx-auto ">
+
+            <ul>
+                <li v-for="(task, index) in tasks" :key="index"
+                    class="flex justify-between  mt-4 items-center bg-gray-100 p-2 mb-2 rounded ">
+                    <span :class="{ 'line-through': task.completed }" @click="toggleTask(index)">
+                        {{ task.text }}
+                    </span>
+
+                </li>
+            </ul>
         </div>
 
     </div>
@@ -15,7 +28,7 @@
 
 
 <script setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
 
 
 
@@ -23,12 +36,12 @@ const newTask = ref('');
 const tasks = ref([]);
 
 const addTask = () => {
-  console.log('newtask', newTask.value)
-    
-  if (newTask.value.trim()) {
-    tasks.value.push({ text: newTask.value, isCompleted: false });
-    newTask.value = '';
-  }
-  console.log(tasks.value)
+    console.log('newtask', newTask.value)
+
+    if (newTask.value.trim()) {
+        tasks.value.push({ text: newTask.value, isCompleted: false });
+        newTask.value = '';
+    }
+    console.log(tasks.value)
 };
 </script>
